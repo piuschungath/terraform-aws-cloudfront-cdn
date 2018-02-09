78ee652516d91c3947972b4ae836bb02f23afc25
pchungath <pius@axistwelve.com>
2018-02-09 11:24:44 +0000

    decouple route53 module from CF

--- main.tf
+++ main.tf
@@ -109,12 +109,3 @@ resource "aws_cloudfront_distribution" "default" {
 
   tags = "${module.distribution_label.tags}"
 }
-
-module "dns" {
-  source           = "git::https://github.com/cloudposse/terraform-aws-route53-alias.git?ref=tags/0.2.2"
-  aliases          = "${var.aliases}"
-  parent_zone_id   = "${var.parent_zone_id}"
-  parent_zone_name = "${var.parent_zone_name}"
-  target_dns_name  = "${aws_cloudfront_distribution.default.domain_name}"
-  target_zone_id   = "${aws_cloudfront_distribution.default.hosted_zone_id}"
-}
